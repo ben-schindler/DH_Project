f25b3afd5cf422cc9ba6d412651e6b08918119a5
Linus <benjaminschindler@bnsn.fritz.box>
2019-02-07 18:40:37 +0100

    added new Topic Tasks

---
+++ croesus.js
@@ -0,0 +1,425 @@
+{
+ "type": "FeatureCollection",
+ "features": [
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Alyattes",
+    "description": "The tumulus of the Lydian king Alyattes.",
+    "count": 6,
+    "featureTypes": "tumulus",
+    "timePeriodsKeys": "archaic,classical",
+    "timePeriodsRange": "-750.0,-330.0",
+    "minDate": -750.0,
+    "maxDate": -330.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     28.044921000000002,
+     38.572215
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Ionia (region)",
+    "description": "The coast of Asia Minor between Aeolis and Caria.",
+    "count": 2,
+    "featureTypes": "region",
+    "timePeriodsKeys": "archaic,classical,hellenistic-republican,roman,late-antique",
+    "timePeriodsRange": "-750.0,640.0",
+    "minDate": -750.0,
+    "maxDate": 640.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     27.3660121333,
+     38.2585728556
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Lydia/Maionia",
+    "description": "Lydia was an Iron Age kingdom of western Asia Minor located east of ancient Ionia. Lydia (called Sparda by the Achaemenids) became a satrapy of the Persian Empire and with Sardis was its capital.",
+    "count": 6,
+    "featureTypes": "region",
+    "timePeriodsKeys": "archaic,classical,hellenistic-republican,roman,late-antique",
+    "timePeriodsRange": "-750.0,640.0",
+    "minDate": -750.0,
+    "maxDate": 640.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     27.963284499999997,
+     38.62477125
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": " Sardis/Hyde?",
+    "description": "Sardis was the capital of the Lydian Empire located in western Turkey. In 188 B.C. Sardis became a part of the Attalid kingdom and thus it passed with the territory of Attalus III to Rome in 133 B.C. ",
+    "count": 20,
+    "featureTypes": "settlement",
+    "timePeriodsKeys": "middle-bronze-age-anatolia,early-iron-age-anatolia,1200-bc-middle-east,archaic,neo-assyrian-babylonian-middle-east,classical,achaemenid-middle-east,hellenistic-republican,hellenistic-middle-east,roman-middle-east,roman,roman-early-empire-parthian-middle-east,transition-roman-early-empire-late-antique,late-antique,late-antique-sasanian-middle-east,late-antique-late-byzantine,proto-byzantine,early-byzantine,abassid-middle-east,middle-byzantine,rum-crusader-anatolia,early-ottoman-empire,modern-middle-east",
+    "timePeriodsRange": "-1750.0,2000.0",
+    "minDate": -1750.0,
+    "maxDate": 2000.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     28.040286,
+     38.488314
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Athenae",
+    "description": "A major Greek city-state and the principal city of Attika. Modern Athens in Greece.",
+    "count": 4,
+    "featureTypes": "settlement",
+    "timePeriodsKeys": "archaic,classical,hellenistic-republican,roman,late-antique,modern",
+    "timePeriodsRange": "-750.0,2100.0",
+    "minDate": -750.0,
+    "maxDate": 2100.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     23.7263451721,
+     37.975176467800004
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Delphi",
+    "description": "The ancient pan-Hellenic sanctuary of Delphi in Greece, seat of the oracle of Apollo.",
+    "count": 6,
+    "featureTypes": "sanctuary, settlement",
+    "timePeriodsKeys": "archaic,classical,hellenistic-republican,roman,late-antique,modern",
+    "timePeriodsRange": "-750.0,2100.0",
+    "minDate": -750.0,
+    "maxDate": 2100.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     22.501169,
+     38.482289
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Sparta",
+    "description": "Sparta was a prominent city-state (polis) of ancient Greece.",
+    "count": 3,
+    "featureTypes": "settlement, temple, temple-2",
+    "timePeriodsKeys": "middle-geometric,archaic,classical,hellenistic-republican,roman",
+    "timePeriodsRange": "-850.0,300.0",
+    "minDate": -850.0,
+    "maxDate": 300.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     22.4272985,
+     37.077905
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Thornax",
+    "description": "An ancient place, cited: BAtlas 58 unlocated Thornax",
+    "count": 1,
+    "featureTypes": "unlocated",
+    "timePeriodsKeys": "archaic,classical",
+    "timePeriodsRange": "-750.0,-330.0",
+    "minDate": -750.0,
+    "maxDate": -330.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     NaN,
+     NaN
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Lacedaemon/Laconia",
+    "description": "Lacedaemon/Laconia is a region comprising the south-eastern part of the Peloponnese.",
+    "count": 1,
+    "featureTypes": "region",
+    "timePeriodsKeys": "archaic,classical,hellenistic-republican,roman",
+    "timePeriodsRange": "-750.0,300.0",
+    "minDate": -750.0,
+    "maxDate": 300.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     22.4608264518,
+     37.0762404208
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Cappadocia",
+    "description": "An ancient place, cited: BAtlas 64 B2 Cappadocia",
+    "count": 3,
+    "featureTypes": "labeled feature",
+    "timePeriodsKeys": "classical,hellenistic-republican,roman,late-antique",
+    "timePeriodsRange": "-550.0,640.0",
+    "minDate": -550.0,
+    "maxDate": 640.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     35.75,
+     39.25
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Halys (river)",
+    "description": "The K\u0131z\u0131l\u0131rmak river, the ancient Halys River, is a major river of Asia Minor that flows into the Black Sea.",
+    "count": 1,
+    "featureTypes": "settlement, river",
+    "timePeriodsKeys": "middle-bronze-age-anatolia,middle-hittite-anatolia,early-iron-age-anatolia,1200-bc-middle-east,middle-late-iron-age-anatolia,classical,achaemenid-middle-east,hellenistic-republican,hellenistic-middle-east,roman-middle-east,roman,roman-early-empire-parthian-middle-east,transition-roman-early-empire-late-antique,late-antique,late-antique-sasanian-middle-east,late-antique-late-byzantine,early-byzantine,abassid-middle-east,middle-byzantine,rum-crusader-anatolia,crusader-byzantine-seljuq-middle-east,ottoman-rise,early-ottoman-empire,perso-ottoman-russian-caucasus,late-ottoman-empire,modern,modern-middle-east",
+    "timePeriodsRange": "-1750.0,2100.0",
+    "minDate": -1750.0,
+    "maxDate": 2100.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     35.9295291,
+     41.6767598
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Pteria",
+    "description": "An ancient place, cited: BAtlas 87 A3 Pteria",
+    "count": 2,
+    "featureTypes": "settlement",
+    "timePeriodsKeys": "classical,late-antique",
+    "timePeriodsRange": "-550.0,640.0",
+    "minDate": -550.0,
+    "maxDate": 640.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     35.108768,
+     41.321531
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Sinope",
+    "description": "Sinope (modern Sinop, Turkey) on the Black Sea has been a major port since the time of the Hittites. ",
+    "count": 1,
+    "featureTypes": "settlement, port",
+    "timePeriodsKeys": "middle-bronze-age-anatolia,1200-bc-middle-east,archaic,middle-late-iron-age-anatolia,classical,achaemenid-middle-east,hellenistic-republican,hellenistic-middle-east,roman-middle-east,roman,roman-early-empire-parthian-middle-east,transition-roman-early-empire-late-antique,late-antique,late-antique-sasanian-middle-east,late-antique-late-byzantine,abassid-middle-east,rum-crusader-anatolia,crusader-byzantine-seljuq-middle-east,ottoman-rise,early-ottoman-empire,1500-ad-middle-east,late-ottoman-empire,ottoman-decline-mandate-middle-east,modern-middle-east",
+    "timePeriodsRange": "-1750.0,2000.0",
+    "minDate": -1750.0,
+    "maxDate": 2000.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     35.143037,
+     42.025776
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Euxine",
+    "description": "An ancient place, cited: BAtlas 65 A4 Euxine",
+    "count": 1,
+    "featureTypes": "settlement",
+    "timePeriodsKeys": "hellenistic-republican",
+    "timePeriodsRange": "-330.0,-30.0",
+    "minDate": -330.0,
+    "maxDate": -30.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     28.715749,
+     36.837326000000004
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Lacedaemon/Laconia",
+    "description": "Lacedaemon/Laconia is a region comprising the south-eastern part of the Peloponnese.",
+    "count": 1,
+    "featureTypes": "region",
+    "timePeriodsKeys": "archaic,classical,hellenistic-republican,roman",
+    "timePeriodsRange": "-750.0,300.0",
+    "minDate": -750.0,
+    "maxDate": 300.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     22.4608264518,
+     37.0762404208
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Thebai/Thebae",
+    "description": "The ancient city of Thebes in Boeotia (modern Greece).",
+    "count": 1,
+    "featureTypes": "settlement",
+    "timePeriodsKeys": "archaic,classical,hellenistic-republican,roman,late-antique,modern",
+    "timePeriodsRange": "-750.0,2100.0",
+    "minDate": -750.0,
+    "maxDate": 2100.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     23.317577,
+     38.319156
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Boeotia",
+    "description": "A region of central Greece inhabited since prehistoric times. ",
+    "count": 1,
+    "featureTypes": "region",
+    "timePeriodsKeys": "classical,roman",
+    "timePeriodsRange": "-550.0,300.0",
+    "minDate": -550.0,
+    "maxDate": 300.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     23.1846775333,
+     38.3466880333
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Babylon",
+    "description": "The ancient city of Babylon began as an Akkadian city-state founded by Amorites in 1894 B.C. The city was also the seat of the Neo-Babylonian Empire from 608 to 539 B.C.",
+    "count": 1,
+    "featureTypes": "urban, settlement",
+    "timePeriodsKeys": "old-babylonian-assyrian-mesopotamia,later-2nd-millennium-bc-mesopotamia,archaic,neo-assyrian-babylonian-middle-east,classical,achaemenid-middle-east,hellenistic-republican,hellenistic-central-asia,roman,late-antique",
+    "timePeriodsRange": "-2000.0,640.0",
+    "minDate": -2000.0,
+    "maxDate": 640.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     44.4236176449,
+     32.5387834636
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Ecbatana/Hagmatana/Achmeta/Epiphaneia/Ahmadan",
+    "description": "The capital of Astyages (Istuveg\u00fc), captured by Cyrus the Great in 549 BC. The site served as a summer residence for both Persian and Parthian kings.",
+    "count": 4,
+    "featureTypes": "settlement",
+    "timePeriodsKeys": "1200-bc-middle-east,archaic,neo-assyrian-babylonian-middle-east,classical,achaemenid-middle-east,hellenistic-republican,hellenistic-middle-east,roman,roman-early-empire-parthian-middle-east,roman-early-empire-late-antique,transition-roman-early-empire-late-antique,late-antique,late-antique-sasanian-middle-east,proto-byzantine,caliphate-umayyad-middle-east,abassid-middle-east,samanid-ghaznavid-iran,seljuq-khwarezmian-middle-east,khwarezmian-middle-east,ilkhanate-middle-east,mongol-middle-east,timurid-middle-east,1500-ad-middle-east,safavid-middle-east,late-ottoman-empire,ottoman-decline-mandate-middle-east,modern-middle-east",
+    "timePeriodsRange": "-1200.0,2000.0",
+    "minDate": -1200.0,
+    "maxDate": 2000.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     48.512209000000006,
+     34.799294
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Anger? (river)",
+    "description": "An ancient place, cited: BAtlas 14 G1 Anger? fl.",
+    "count": 1,
+    "featureTypes": "river",
+    "timePeriodsKeys": "late-antique,modern",
+    "timePeriodsRange": "300.0,2100.0",
+    "minDate": 300.0,
+    "maxDate": 2100.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     1.6846846999999998,
+     46.812987799999995
+    ]
+   }
+  },
+  {
+   "type": "Feature",
+   "properties": {
+    "title": "Pityoussa/Lampsacus",
+    "description": "A Greek city located on the eastern side of the Hellespont. ",
+    "count": 1,
+    "featureTypes": "settlement",
+    "timePeriodsKeys": "archaic,classical,hellenistic-republican,roman",
+    "timePeriodsRange": "-750.0,300.0",
+    "minDate": -750.0,
+    "maxDate": 300.0
+   },
+   "geometry": {
+    "type": "Point",
+    "coordinates": [
+     26.699162,
+     40.346685
+    ]
+   }
+  }
+ ]
+}
